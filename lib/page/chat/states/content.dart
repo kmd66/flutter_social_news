a615c93f744af5634854791a283cbf1a85353ffb
kmd66 <105439395+kmd66@users.noreply.github.com>
2022-05-31 10:47:59 +0430

    .

--- lib/page/chat/states/content.dart
+++ lib/page/chat/states/content.dart
@@ -54,25 +54,27 @@ class _Content extends State<Content> {
               children: [
                 Column(children: [
                   hed(context,size.width),
+
                   Expanded(
-                    child:
-                    Scrollbar(
-                      child:RawScrollbar(
-                        isAlwaysShown: true,
-                        thumbColor:ObjectColor.shadowBackground(.2),
-                        radius: Radius.circular(20),
-                        thickness: 10,
-                        child:SingleChildScrollView(
-                          child: Padding(
-                            padding: const EdgeInsets.all(12),
-                            child: Column(children: [
-                              Text('You have pressed the button  times.'),
-                            ]),
-                          )
+                        child:
+                        Scrollbar(
+                          child:RawScrollbar(
+                            isAlwaysShown: true,
+                            thumbColor:ObjectColor.shadowBackground(.2),
+                            radius: Radius.circular(20),
+                            thickness: 10,
+                            child:SingleChildScrollView(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(12),
+                                  child: Column(children:
+                                  list.map((model) => item(size.width, model)).toList()
+                                  ),
+                                )
+                            ),
+                          ),
                         ),
-                      ),
                     ),
-                  ),
+
                   btn(context),
                 ],),
               ]),
@@ -132,54 +134,32 @@ class _Content extends State<Content> {
       );
   }
 
-  Widget item(BuildContext context, BoxConstraints constraints, int i) {
-    double _imgSize = 25;
-    const EdgeInsets p1 =  EdgeInsets.symmetric(horizontal: 12.0);
-
-    const EdgeInsets m1 =  EdgeInsets.symmetric(vertical: 12.0);
-
-    return
-      Row(
-        children: [
-          Container(
-            margin: m1,
-            padding:p1 ,
-            child: CircleAvatar(
-              radius: _imgSize,
-              backgroundColor: ObjectColor.base ,
-              child: CircleAvatar(
-                child: ClipOval(child:GetImage(list[i].Img)),
-                radius: _imgSize - 1,
-              ),
+  Widget item(double size, ChatModel model) {
+    return Container(
+      width: size,
+      child: Align(
+        alignment: model.b ? Alignment.topLeft:Alignment.topRight,
+        child:Container(
+            padding: const EdgeInsets.all(7.0),
+            margin: const EdgeInsets.all( 4.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+              color: model.b ?teal:blueGrey,
             ),
-          ),
-
-          Expanded(
+            constraints:  BoxConstraints(maxWidth: ((size *70 )/100)),
             child:
-            Container(
-              // width: constraints.maxWidth -(p1.left +p1.right + (_imgSize*2)),
-              child:
-              Column(
-                crossAxisAlignment : CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment : MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text( list[i].Name, style: Style.h4()),
-                      Text( list[i].Date, style: Style.h6()),
-                    ],),
-                  Text( list[i].Msg, style: Style.h6(overflow: TextOverflow.ellipsis)),
-                ],),
-            ),
-          ),
-
-          IconButton(
-              icon: Icon(Icons.more_vert,color: ObjectColor.baseTextColor,),
-              onPressed: (){setState(() {});
-              }),
-
-        ],
-      );
+            Stack(
+                children : [
+                  Text('${model.Msg} - ${model.Date}',style: Style.h5(color: white),),
+                  Positioned(
+                      left: model.b? 0:null,
+                      child: CustomPaint(painter: ChatBubbleTriangle(model.b),)
+                  )
+                ]
+            )
+        ),
+      ),
+    );
   }
 
   Widget btn(BuildContext context) {
@@ -196,6 +176,7 @@ class _Content extends State<Content> {
                 borderRadius: BorderRadius.all(const Radius.circular(20.0)),
               ),
               child: Row(children: [
+
                 IconButton(
                     icon:Icon(Icons.send, color: ObjectColor.baseTextColor , size: 16,),
                     onPressed: () {
@@ -209,6 +190,7 @@ class _Content extends State<Content> {
                     onPressed: () {
                     }
                 ),
+
               ],) ,
             ),
           ),
@@ -233,23 +215,21 @@ class _Content extends State<Content> {
   void listAdd() {
     Random re = new Random();
     List<String> Msg =[
-      'دکمه سبز رنگ ',
-      'دکمه سبز رنگ ',
+      'دکمه سبز رنگ ',' connection from deb','Performing hot restart...','estarted application',
       'مخفی کردن فالویینگ. بنابراین، ما می‌دانیم که نگرانی شما این است که از',
       ' مون چقدر باشد؟ اولین دیدگاه غلط اینه که افراد فکر میکنند با اول ',
       'در این مطلب تفاوت های فالوور و فال',
     ];
+    var now = new DateTime.now();
 
-    for (int i = 0; i < 50; i++) {
-      int b =re.nextInt(1);
-      list.add(
-          ChatModel.setProperty(
-              Msg: Msg[re.nextInt(Msg.length - 1)],
-              b: b==1?true:false
-          )
-      );
+    list.add(ChatModel.setProperty(Msg: 'پیام اول', b: true,Date: now.toString() ));
+    for (int i = 0; i < 70; i++) {
+      DateTime d = now.add(Duration(minutes: -1*i));
+      int b =re.nextInt(2);
+      list.add(ChatModel.setProperty(Msg: Msg[re.nextInt(Msg.length - 1)],b: b==1?true:false,Date: d.toString()));
 
     }
+    list.add(ChatModel.setProperty(Msg: 'پیام آخر',b: true,Date: now.add(Duration(days: -70)).toString()));
   }
 
   Widget input() {
@@ -269,4 +249,27 @@ class _Content extends State<Content> {
    );
   }
 
+}
+class ChatBubbleTriangle extends CustomPainter {
+  final bool b ;
+
+  ChatBubbleTriangle(this.b);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double i = b? -10:10;
+    var paint = Paint()..color = b ?teal:blueGrey ;//
+
+    var path = Path();
+    path.lineTo(i, -7);
+    path.lineTo(0, 0);
+    path.lineTo(0, -7);
+    path.lineTo(i, -7);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
 }
